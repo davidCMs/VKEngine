e88fbebe9948e8a48865a97720f67064a99952c8
davidCMs <gregor.cink@gmail.com>
2025-05-06 20:34:44 +0200

    Changed off of a deprecated thingy

--- src/main/java/org/davidCMs/vkengine/vk/VkEVersion.java
+++ src/main/java/org/davidCMs/vkengine/vk/VkEVersion.java
@@ -1,4 +1,11 @@
 package org.davidCMs.vkengine.vk;
 
-public record VkEVersion(int major, int minor, int patch) {
+import org.lwjgl.vulkan.VK14;
+
+public record VkEVersion(int variant, int major, int minor, int patch) {
+
+	public int makeVersion() {
+		return VK14.VK_MAKE_API_VERSION(variant, major, minor, patch);
+	}
+
 }
