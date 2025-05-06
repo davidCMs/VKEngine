df9cc84ed64885e15a22c9c1da97321beaed1466
davidCMs <gregor.cink@gmail.com>
2025-05-06 20:36:11 +0200

    Refactored to remove the MemoryStack

--- src/main/java/org/davidCMs/vkengine/vk/VkEInstanceCreateInfo.java
+++ src/main/java/org/davidCMs/vkengine/vk/VkEInstanceCreateInfo.java
@@ -3,15 +3,15 @@ package org.davidCMs.vkengine.vk;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.system.windows.MSG;
 import org.lwjgl.vulkan.*;
 
 import java.nio.ByteBuffer;
 import java.util.HashSet;
 import java.util.Set;
 
-public class VkEInstanceCreateInfo implements AutoCloseable {
+public class VkEInstanceCreateInfo extends AutoCloseableResource {
 
-	private final MemoryStack stack;
 	private final org.lwjgl.vulkan.VkInstanceCreateInfo info;
 	private final VkDebugUtilsMessengerCreateInfoEXT messengerInfo;
 
@@ -33,44 +33,50 @@ public class VkEInstanceCreateInfo implements AutoCloseable {
 	private VkEInternalDebugMessengerCallback internalMessengerCallback = new VkEInternalDebugMessengerCallback(messengerCallback);
 
 	public VkEInstanceCreateInfo() {
-		stack = MemoryStack.stackPush();
-		info = org.lwjgl.vulkan.VkInstanceCreateInfo.calloc(stack);
+		info = org.lwjgl.vulkan.VkInstanceCreateInfo.calloc();
 		info.sType$Default();
-		messengerInfo = VkDebugUtilsMessengerCreateInfoEXT.calloc(stack);
+		messengerInfo = VkDebugUtilsMessengerCreateInfoEXT.calloc();
 		messengerInfo.sType$Default();
 		info.pNext(messengerInfo);
 		messengerInfo.pfnUserCallback(internalMessengerCallback);
 	}
 
 	public VkEInstanceCreateInfo setApplicationCreateInfo(VkEApplicationInfo appInfo) {
+		check();
 		info.pApplicationInfo(appInfo.getInfo());
 		return this;
 	}
 
 	public VkEInstanceCreateInfo setEnabledLayerNames(String... layers) {
+		return setEnabledLayerNames(Set.of(layers));
+	}
+
+	public VkEInstanceCreateInfo setEnabledLayerNames(Set<String> layers) {
+		check();
+		if (info.ppEnabledLayerNames() != null)
+			freeLayerNames();
+
 		for (String name : layers) {
 			if (!VkELayerUtils.checkAvailabilityOf(name))
 				throw new VkELayerNotFoundException("Layer \"" + name + "\" not found.");
 		}
 
-		PointerBuffer namesPtr = stack.mallocPointer(layers.length);
+		PointerBuffer namesPtr = MemoryUtil.memAllocPointer(layers.size());
 
 		for (String name : layers) {
-			ByteBuffer buf = stack.UTF8(name);
+			ByteBuffer buf = MemoryUtil.memUTF8(name);
 			namesPtr.put(buf);
 		}
 
 		namesPtr.flip();
-
 		info.ppEnabledLayerNames(namesPtr);
-
 		return this;
 	}
 
 	public Set<String> getEnabledLayerNames() {
+		check();
 		PointerBuffer namesPtr = info.ppEnabledLayerNames();
 		if (namesPtr == null) {
-			System.out.println("No layers enabled");
 			return Set.of();
 		}
 
@@ -84,31 +90,37 @@ public class VkEInstanceCreateInfo implements AutoCloseable {
 		return names;
 	}
 
-	public VkEInstanceCreateInfo setEnabledExtensionsNames(String... extensions) {
+	public VkEInstanceCreateInfo setEnabledExtensionNames(String extension) {
+		return setEnabledExtensionNames(Set.of(extension));
+	}
+
+	public VkEInstanceCreateInfo setEnabledExtensionNames(Set<String> extensions) {
+		check();
+		if (info.ppEnabledExtensionNames() != null)
+			freeExtensionNames();
+
 		for (String name : extensions) {
 			if (!VkEExtensionUtils.checkAvailabilityOf(name)) {
 				throw new VkEExtensionNotFoundException("Extension \"" + name + "\" not found.");
 			}
 		}
 
-		PointerBuffer namesPtr = stack.mallocPointer(extensions.length);
+		PointerBuffer namesPtr = MemoryUtil.memAllocPointer(extensions.size());
 
 		for (String name : extensions) {
-			ByteBuffer buf = stack.UTF8(name);
+			ByteBuffer buf = MemoryUtil.memUTF8(name);
 			namesPtr.put(buf);
 		}
 
 		namesPtr.flip();
-
 		info.ppEnabledExtensionNames(namesPtr);
-
 		return this;
 	}
 
 	public Set<String> getEnabledExtensionNames() {
+		check();
 		PointerBuffer namesPtr = info.ppEnabledExtensionNames();
 		if (namesPtr == null) {
-			System.out.println("No extensions enabled");
 			return Set.of();
 		}
 
@@ -123,29 +135,80 @@ public class VkEInstanceCreateInfo implements AutoCloseable {
 	}
 
 	public VkEInstanceCreateInfo setDebugMessageTypes(VkEDebugMessageType... types) {
+		check();
 		messengerInfo.messageType(VkEDebugMessageType.getValueOf(types));
 		return this;
 	}
 
 	public VkEInstanceCreateInfo setDebugMessageSeverity(VkEDebugMessageSeverity... severities) {
+		check();
 		messengerInfo.messageSeverity(VkEDebugMessageSeverity.getValueOf(severities));
 		return this;
 	}
 
 	public VkEInstanceCreateInfo setMessengerCallback(VkEDebugMessengerCallback callback) {
+		check();
 		this.messengerCallback = callback;
 		return this;
 	}
 
-	org.lwjgl.vulkan.VkInstanceCreateInfo getInfo() {return info;}
-	VkDebugUtilsMessengerCreateInfoEXT getMessengerInfo() {return messengerInfo;}
-	VkEInternalDebugMessengerCallback getInternalMessengerCallback() {return internalMessengerCallback;}
-	void setInternalMessengerCallback (VkEInternalDebugMessengerCallback callback) {internalMessengerCallback = callback;}
+	org.lwjgl.vulkan.VkInstanceCreateInfo getInfo() {
+		check();
+		return info;
+	}
+
+	VkDebugUtilsMessengerCreateInfoEXT getMessengerInfo() {
+		check();
+		return messengerInfo;
+	}
+
+	VkEInternalDebugMessengerCallback getInternalMessengerCallback() {
+		check();
+		return internalMessengerCallback;
+	}
+
+	void setInternalMessengerCallback (VkEInternalDebugMessengerCallback callback) {
+		check();
+		internalMessengerCallback = callback;
+	}
+
+	private void freeLayerNames() {
+		PointerBuffer namesPtr = info.ppEnabledLayerNames();
+
+		if (namesPtr == null)
+			throw new IllegalStateException("Cannot free layer names as there is nothing to be freed");
+
+		for (int i = 0; i < namesPtr.remaining(); i++) {
+			MemoryUtil.nmemFree(namesPtr.get(i));
+		}
+
+		MemoryUtil.memFree(namesPtr);
+	}
+
+	private void freeExtensionNames() {
+		PointerBuffer namesPtr = info.ppEnabledExtensionNames();
+
+		if (namesPtr == null)
+			throw new IllegalStateException("Cannot free extension names as there is nothing to be freed");
+
+		for (int i = 0; i < namesPtr.remaining(); i++) {
+			MemoryUtil.nmemFree(namesPtr.get(i));
+		}
+
+		MemoryUtil.memFree(namesPtr);
+	}
 
 	@Override
-	public void close() throws Exception {
+	public void close() {
+		super.close();
+
+		try {
+			freeLayerNames();
+			freeExtensionNames();
+		} catch (IllegalStateException ignored) {}
+		info.close();
+
 		if (internalMessengerCallback != null)
 			internalMessengerCallback.close();
-		stack.close();
 	}
 }
