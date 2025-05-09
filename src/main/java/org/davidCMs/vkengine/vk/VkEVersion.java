8a5738b5e69ce83c17239316ae073ebd483e902c
davidCMs <gregor.cink@gmail.com>
2025-05-09 18:53:29 +0200

    Added a constructor to that constructs from a vulkan api version int

--- src/main/java/org/davidCMs/vkengine/vk/VkEVersion.java
+++ src/main/java/org/davidCMs/vkengine/vk/VkEVersion.java
@@ -4,6 +4,15 @@ import org.lwjgl.vulkan.VK14;
 
 public record VkEVersion(int variant, int major, int minor, int patch) {
 
+	public VkEVersion(int verInt) {
+		this(
+				VK14.VK_API_VERSION_VARIANT(verInt),
+				VK14.VK_API_VERSION_MAJOR(verInt),
+				VK14.VK_API_VERSION_MINOR(verInt),
+				VK14.VK_API_VERSION_PATCH(verInt)
+		);
+	}
+
 	public int makeVersion() {
 		return VK14.VK_MAKE_API_VERSION(variant, major, minor, patch);
 	}
