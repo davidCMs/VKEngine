9429ca7794a7e1f28ef5420b5636316b85810ac6
davidCMs <gregor.cink@gmail.com>
2025-05-11 21:28:25 +0200

    implemented wrapper for VkDeviceCreateInfo

---
+++ src/main/java/org/davidCMs/vkengine/vk/VkEDeviceCreateInfo.java
@@ -0,0 +1,140 @@
+package org.davidCMs.vkengine.vk;
+
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.vulkan.VkDeviceCreateInfo;
+import org.lwjgl.vulkan.VkDeviceQueueCreateInfo;
+
+import java.nio.ByteBuffer;
+import java.util.HashSet;
+import java.util.Set;
+
+public class VkEDeviceCreateInfo extends AutoCloseableResource {
+
+	private final VkDeviceCreateInfo info;
+
+	private final Set<VkEDeviceQueueCreateInfo> queueCreateInfos = new HashSet<>();
+	private final Set<String> extensionNames = new HashSet<>();
+	private final Set<String> layerNames = new HashSet<>();
+
+	public VkEDeviceCreateInfo() {
+		info = VkDeviceCreateInfo.calloc();
+		info.sType$Default();
+
+		info.pQueueCreateInfos(VkDeviceQueueCreateInfo.calloc(0));
+		info.ppEnabledExtensionNames(MemoryUtil.memAllocPointer(0));
+		info.ppEnabledLayerNames(MemoryUtil.memAllocPointer(0));
+	}
+
+	public VkEDeviceCreateInfo setEnabledLayers(Set<String> names) {
+		check();
+		if (info.ppEnabledLayerNames() != null)
+			clearLayerBuffer();
+		PointerBuffer buffer = MemoryUtil.memCallocPointer(names.size());
+
+		int i = 0;
+
+		for (String s : names) {
+			ByteBuffer buf = MemoryUtil.memUTF8(s);
+			buffer.put(i, buf);
+			i++;
+		}
+
+		info.ppEnabledLayerNames(buffer);
+		layerNames.clear();
+		layerNames.addAll(names);
+		return this;
+	}
+
+	public Set<String> getEnabledLayers() {
+		check();
+		return layerNames;
+	}
+
+	public VkEDeviceCreateInfo setEnabledExtensions(Set<String> names) {
+		check();
+		if (info.ppEnabledExtensionNames() != null)
+			clearExtensionBuffer();
+		PointerBuffer buffer = MemoryUtil.memCallocPointer(names.size());
+
+		int i = 0;
+
+		for (String s : names) {
+			ByteBuffer buf = MemoryUtil.memUTF8(s);
+			buffer.put(i, buf);
+			i++;
+		}
+
+		info.ppEnabledExtensionNames(buffer);
+		extensionNames.clear();
+		extensionNames.addAll(names);
+		return this;
+	}
+
+	public Set<String> getEnabledExtensions() {
+		check();
+		return extensionNames;
+	}
+
+	public VkEDeviceCreateInfo setQueueCreateInfos(Set<VkEDeviceQueueCreateInfo> infos) {
+		check();
+		clearQueueInfosBuffer();
+		VkDeviceQueueCreateInfo.Buffer buffer = VkDeviceQueueCreateInfo.malloc(infos.size());
+
+		int i = 0;
+		for (VkEDeviceQueueCreateInfo info : infos) {
+			buffer.put(i, info.getInfo());
+			i++;
+		}
+
+		info.pQueueCreateInfos(buffer);
+
+		queueCreateInfos.clear();
+		queueCreateInfos.addAll(infos);
+		return this;
+	}
+
+	public Set<VkEDeviceQueueCreateInfo> getQueueCreateInfos() {
+		check();
+		if (info.pQueueCreateInfos() == null) return Set.of();
+		return queueCreateInfos;
+	}
+
+	private void clearQueueInfosBuffer() {
+		if (info.pQueueCreateInfos() == null) return;
+		VkDeviceQueueCreateInfo.Buffer buffer = info.pQueueCreateInfos();
+		for (int i = 0; i < buffer.remaining(); i++) {
+			buffer.get(i).close();
+		}
+		MemoryUtil.memFree(buffer);
+	}
+
+	private void clearExtensionBuffer() {
+		if (info.ppEnabledExtensionNames() == null) return;
+		PointerBuffer buffer = info.ppEnabledExtensionNames();
+		for (int i = 0; i < buffer.remaining(); i++) {
+			MemoryUtil.nmemFree(buffer.get(i));
+		}
+		MemoryUtil.memFree(buffer);
+	}
+
+	private void clearLayerBuffer() {
+		if (info.ppEnabledLayerNames() == null) return;
+		PointerBuffer buffer = info.ppEnabledLayerNames();
+		for (int i = 0; i < buffer.remaining(); i++) {
+			MemoryUtil.nmemFree(buffer.get(i));
+		}
+		MemoryUtil.memFree(buffer);
+	}
+
+	@Override
+	public void close() {
+		super.close();
+
+		clearQueueInfosBuffer();
+		clearExtensionBuffer();
+		clearLayerBuffer();
+
+		info.close();
+	}
+}
---
+++ src/test/java/org/davidCMs/vkengine/vk/VkEDeviceCreateInfoTest.java
@@ -0,0 +1,101 @@
+package org.davidCMs.vkengine.vk;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+class VkEDeviceCreateInfoTest {
+
+	@Test
+	void getQueueCreateInfosCloseCheck() {
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo();
+		info.close();
+
+		Assertions.assertThrows(ClosedResourceException.class, info::getQueueCreateInfos);
+	}
+
+	@Test
+	void getEnabledExtensionsCloseCheck() {
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo();
+		info.close();
+
+		Assertions.assertThrows(ClosedResourceException.class, info::getEnabledExtensions);
+	}
+
+	@Test
+	void getEnabledLayersCloseCheck() {
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo();
+		info.close();
+
+		Assertions.assertThrows(ClosedResourceException.class, info::getEnabledLayers);
+	}
+
+	@Test
+	void setQueueCreateInfosCloseCheck() {
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo();
+		info.close();
+
+		VkEQueueFamily family = VkEDeviceQueueCreateInfoTest.getFamily();
+
+		Assertions.assertThrows(ClosedResourceException.class,
+				() -> info.setQueueCreateInfos(Set.of(family.makeCreateInfo(1))));
+	}
+
+	@Test
+	void setEnabledExtensionsCloseCheck() {
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo();
+		info.close();
+
+		Assertions.assertThrows(ClosedResourceException.class,
+				() -> info.setEnabledExtensions(Set.of("A")));
+	}
+
+	@Test
+	void setEnabledLayersCloseCheck() {
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo();
+		info.close();
+
+		Assertions.assertThrows(ClosedResourceException.class,
+				() -> info.setEnabledLayers(Set.of("A")));
+	}
+
+	@Test
+	void queueCreateInfosSetGetTest() {
+		VkEQueueFamily family = VkEDeviceQueueCreateInfoTest.getFamily();
+
+		Set<VkEDeviceQueueCreateInfo> set = Set.of(family.makeCreateInfo(1));
+
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo()
+				.setQueueCreateInfos(set);
+
+		Set<VkEDeviceQueueCreateInfo> gottenSet = info.getQueueCreateInfos();
+
+		Assertions.assertEquals(set, gottenSet);
+	}
+
+	@Test
+	void extensionsSetGetTest() {
+		Set<String> set = Set.of("TEST");
+
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo()
+				.setEnabledExtensions(set);
+
+		Set<String> gottenSet = info.getEnabledExtensions();
+
+		Assertions.assertEquals(set, gottenSet);
+	}
+
+	@Test
+	void layersGetTest() {
+		Set<String> set = Set.of("TEST");
+
+		VkEDeviceCreateInfo info = new VkEDeviceCreateInfo()
+				.setEnabledLayers(set);
+
+		Set<String> gottenSet = info.getEnabledLayers();
+
+		Assertions.assertEquals(set, gottenSet);
+	}
+
+}
