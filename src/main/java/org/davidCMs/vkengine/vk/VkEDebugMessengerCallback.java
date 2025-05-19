5fe08c148501bd3e04096887ba1f6d1cf95c91a4
davidCMs <gregor.cink@gmail.com>
2025-05-19 15:51:02 +0200

    Moved default messenger callback from InstanceCreateInfo to the interface itself

--- src/main/java/org/davidCMs/vkengine/vk/VkEDebugMessengerCallback.java
+++ src/main/java/org/davidCMs/vkengine/vk/VkEDebugMessengerCallback.java
@@ -5,6 +5,20 @@ import org.lwjgl.vulkan.VkDebugUtilsMessengerCallbackDataEXT;
 @FunctionalInterface
 public interface VkEDebugMessengerCallback {
 
+	VkEDebugMessengerCallback defaultCallBack = ((severity, type, data) -> {
+		String s = "[Vulkan] [" + type + "] [" + severity + "] " + data.pMessageString();
+
+		switch (severity) {
+			case INFO:
+			case WARNING:
+			case VERBOSE:
+				System.out.println(s);
+				break;
+			case ERROR:
+				System.err.println(s);
+		}
+	});
+
 	void invoke(VkEDebugMessageSeverity severity, VkEDebugMessageType type, VkDebugUtilsMessengerCallbackDataEXT data);
 
 }
