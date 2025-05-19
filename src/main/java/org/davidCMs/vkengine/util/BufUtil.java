cbc65672ec61cce6650f7c123d9f33bdd3305d7b
davidCMs <gregor.cink@gmail.com>
2025-05-19 18:30:43 +0200

    Added method overload for stringsToPointerBuffer that takes a collection

--- src/main/java/org/davidCMs/vkengine/util/BufUtil.java
+++ src/main/java/org/davidCMs/vkengine/util/BufUtil.java
@@ -1,22 +1,28 @@
 package org.davidCMs.vkengine.util;
 
 import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
+import java.util.Collection;
 
 public class BufUtil {
 
-	public static PointerBuffer stringsToPointerBuffer(String... strings) {
-		PointerBuffer ptr = MemoryUtil.memAllocPointer(strings.length);
+	public static PointerBuffer stringsToPointerBuffer(MemoryStack stack, String... strings) {
+		PointerBuffer ptr = stack.callocPointer(strings.length);
 
 		for (String name : strings) {
-			ByteBuffer buf = MemoryUtil.memUTF8(name);
+			ByteBuffer buf = stack.UTF8(name);
 			ptr.put(buf);
 		}
 
 		ptr.flip();
 		return ptr;
+	}
+
+	public static PointerBuffer stringsToPointerBuffer(MemoryStack stack, Collection<String> strings) {
+		return stringsToPointerBuffer(stack, strings.toArray(strings.toArray(new String[0])));
 	}
 
 }
