0c6d28bba8483a0604c766cdafcc33d96ec3a2f6
davidcms <gregor.cink@gmail.com>
2025-05-25 22:46:26 +0200

    Renamed BufUtil tp VkUtil and added a Vector2iToExtent2D method to it

--- src/main/java/org/davidCMs/vkengine/util/VkUtil.java
+++ src/main/java/org/davidCMs/vkengine/util/VkUtil.java
@@ -1,13 +1,14 @@
 package org.davidCMs.vkengine.util;
 
+import org.joml.Vector2i;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.system.MemoryStack;
-import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.vulkan.VkExtent2D;
 
 import java.nio.ByteBuffer;
 import java.util.Collection;
 
-public class BufUtil {
+public class VkUtil {
 
 	public static PointerBuffer stringsToPointerBuffer(MemoryStack stack, String... strings) {
 		PointerBuffer ptr = stack.callocPointer(strings.length);
@@ -23,6 +24,10 @@ public class BufUtil {
 
 	public static PointerBuffer stringsToPointerBuffer(MemoryStack stack, Collection<String> strings) {
 		return stringsToPointerBuffer(stack, strings.toArray(strings.toArray(new String[0])));
+	}
+
+	public static VkExtent2D Vector2iToExtent2D(Vector2i vec, MemoryStack stack) {
+		return VkExtent2D.calloc(stack).set(vec.x, vec.y);
 	}
 
 }
