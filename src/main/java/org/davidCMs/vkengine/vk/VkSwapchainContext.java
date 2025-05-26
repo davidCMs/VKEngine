96e1be51a5ad46ffd402f84373131534d41744b3
davidcms <gregor.cink@gmail.com>
2025-05-26 19:10:00 +0200

    Added rebuild method that recreates the swapchain from the builder, Added destroy method that destroys the swapchain, added getters

--- src/main/java/org/davidCMs/vkengine/vk/VkSwapchainContext.java
+++ src/main/java/org/davidCMs/vkengine/vk/VkSwapchainContext.java
@@ -1,13 +1,30 @@
 package org.davidCMs.vkengine.vk;
 
+import org.lwjgl.vulkan.KHRSwapchain;
+
 public class VkSwapchainContext {
 
-    private long swapchain;
+    private long swapchain = 0;
     private final VkSwapchainBuilder builder;
 
     public VkSwapchainContext(VkSwapchainBuilder builder) {
         this.builder = builder;
+        rebuild();
+    }
+
+    public void rebuild() {
         swapchain = builder.build(swapchain);
     }
 
+    public long getSwapchain() {
+        return swapchain;
+    }
+
+    public VkSwapchainBuilder getBuilder() {
+        return builder;
+    }
+
+    public void destroy() {
+        KHRSwapchain.vkDestroySwapchainKHR(builder.getDevice(), swapchain, null);
+    }
 }
