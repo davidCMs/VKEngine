306c36dafaeeead57e2cdc840d5c056403768459
davidCMs <gregor.cink@gmail.com>
2025-05-29 20:04:24 +0200

    Deleted unused classes

--- src/main/java/org/davidCMs/vkengine/vk/VkCannotCreateQueueException.java
+++
@@ -1,7 +0,0 @@
-package org.davidCMs.vkengine.vk;
-
-public class VkCannotCreateQueueException extends RuntimeException {
-	public VkCannotCreateQueueException(String message) {
-		super(message);
-	}
-}
