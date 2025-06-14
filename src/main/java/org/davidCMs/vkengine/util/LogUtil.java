0cf0e8df8126bbbc2a05ee5e48005703a9ae2663
davidCMs <gregor.cink@gmail.com>
2025-06-14 18:42:55 +0200

    Fixed LogUtil printing strings across multiple lines

--- src/main/java/org/davidCMs/vkengine/util/LogUtil.java
+++ src/main/java/org/davidCMs/vkengine/util/LogUtil.java
@@ -13,37 +13,42 @@ public class LogUtil {
 		StringBuilder b = new StringBuilder();
 
 		int indent = 0;
+		boolean inString = false;
 		for (int i = 0; i < chars.length; i++) {
 			char cc = chars[i];
 			char bc = (i == 0) ? '\u200B' : chars[i-1];
 			char ac = (i == chars.length-1) ? '\u200B' : chars[i+1];
 
-			switch (cc) {
-				case '[', '{' -> {
-					indent++;
-					b.append(" [\n" + "\t".repeat(indent));
-				}
-				case ',' -> {
-					b.append(",\n" + "\t".repeat(indent));
-				}
-				case ']', '}' -> {
-					indent--;
-					b.append("\n" + "\t".repeat(indent) + "]");
-				}
-				case '=' -> {
-					if (bc != ' ')
-						b.append(" ");
-					b.append("=");
-					if (ac != ' ')
-						b.append(" ");
-				}
+			if (cc == '"') inString = !inString;
 
-				default -> {
-					if (cc == ' ' && bc == ',')
-						break;
-					b.append(cc);
+			if (!inString) {
+				switch (cc) {
+					case '[', '{' -> {
+						indent++;
+						b.append(" [\n" + "\t".repeat(indent));
+					}
+					case ',' -> {
+						b.append(",\n" + "\t".repeat(indent));
+					}
+					case ']', '}' -> {
+						indent--;
+						b.append("\n" + "\t".repeat(indent) + "]");
+					}
+					case '=' -> {
+						if (bc != ' ')
+							b.append(" ");
+						b.append("=");
+						if (ac != ' ')
+							b.append(" ");
+					}
+
+					default -> {
+						if (cc == ' ' && bc == ',')
+							break;
+						b.append(cc);
+					}
 				}
-			}
+			} else b.append(cc);
 		}
 		return b.toString();
 	}
