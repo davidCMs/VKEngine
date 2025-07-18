6294a46ad2e7cd7f6ee61d848055dd3a5796557a
davidCMs <gregor.cink@gmail.com>
2025-07-18 23:51:32 +0200

    Implemented waitForFences method and resetFences method and their overloads

--- src/main/java/org/davidCMs/vkengine/vk/VkFence.java
+++ src/main/java/org/davidCMs/vkengine/vk/VkFence.java
@@ -6,9 +6,11 @@ import org.lwjgl.vulkan.VK14;
 import org.lwjgl.vulkan.VkFenceCreateInfo;
 
 import java.nio.LongBuffer;
+import java.util.List;
 import java.util.Set;
 
 public class VkFence {
+    private final VkDeviceContext device;
     private final long fence;
 
     public VkFence(VkDeviceContext device) {
@@ -41,6 +43,7 @@ public class VkFence {
     }
 
     private VkFence(VkDeviceContext device, PNextChainable pNext, int flags) {
+        this.device = device;
         try (MemoryStack stack = MemoryStack.stackPush()) {
             VkFenceCreateInfo info = VkFenceCreateInfo.calloc(stack);
             info.sType$Default();
@@ -59,7 +62,115 @@ public class VkFence {
         }
     }
 
+    private static LongBuffer fencesToLB(MemoryStack stack, List<VkFence> fences) {
+        LongBuffer lb = stack.mallocLong(fences.size());
+        for (int i = 0; i < fences.size(); i++) {
+            lb.put(i, fences.get(i).fence);
+        }
+        return lb;
+    }
+
+    private static LongBuffer fencesToLB(MemoryStack stack, VkFence[] fences) {
+        LongBuffer lb = stack.mallocLong(fences.length);
+        for (int i = 0; i < fences.length; i++) {
+            lb.put(i, fences[i].fence);
+        }
+        return lb;
+    }
+
+    public void reset() {
+        resetFences(this);
+    }
+
+    public static void resetFences(List<VkFence> fences) {
+        if (fences == null || fences.isEmpty())
+            return;
+
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            VK14.vkResetFences(
+                    fences.getFirst().device.device(),
+                    fencesToLB(stack, fences)
+            );
+        }
+    }
+
+    public static void resetFences(VkFence... fences) {
+        if (fences == null || fences.length < 1)
+            return;
+
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            VK14.vkResetFences(
+                    fences[0].device.device(),
+                    fencesToLB(stack, fences)
+            );
+        }
+    }
+
+    public void waitFor() {
+        waitForFences(this);
+    }
+
+    public void waitFor(long timeout) {
+        waitForFences(timeout, this);
+    }
+
+    public static void waitForFences(List<VkFence> fences) {
+        waitForFences(-1, true, fences);
+    }
+
+    public static void waitForFences(VkFence... fences) {
+        waitForFences(-1, true, fences);
+    }
+
+    public static void waitForFences(boolean waitAll, List<VkFence> fences) {
+        waitForFences(-1, waitAll, fences);
+    }
+
+    public static void waitForFences(boolean waitAll, VkFence... fences) {
+        waitForFences(-1, waitAll, fences);
+    }
+
+    public static void waitForFences(long timeout, List<VkFence> fences) {
+        waitForFences(timeout, true, fences);
+    }
+
+    public static void waitForFences(long timeout, VkFence... fences) {
+        waitForFences(timeout, true, fences);
+    }
+
+    public static void waitForFences(long timeout, boolean waitAll, List<VkFence> fences) {
+        if (fences == null || fences.isEmpty())
+            return;
+
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            VK14.vkWaitForFences(
+                    fences.getFirst().device.device(),
+                    fencesToLB(stack, fences),
+                    waitAll,
+                    timeout
+            );
+        }
+    }
+
+    public static void waitForFences(long timeout, boolean waitAll, VkFence... fences) {
+        if (fences == null || fences.length < 1)
+            return;
+
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            VK14.vkWaitForFences(
+                    fences[0].device.device(),
+                    fencesToLB(stack, fences),
+                    waitAll,
+                    timeout
+            );
+        }
+    }
+
     long getFence() {
         return fence;
+    }
+
+    public VkDeviceContext getDevice() {
+        return device;
     }
 }
